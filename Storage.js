51dc2729a22e2c37c2b6392df7de7687ac5b232f
sunga12 <sungabt@gmail.com>
2023-02-02 17:48:39 +0200

    Add local storage syntax to storage file

--- Storage.js
+++ Storage.js
@@ -1,3 +1,14 @@
 const inputemail = document.getElementById('email');
 const inputname = document.getElementById('name');
 const formst = document.getElementById('form');
+
+const Localstore = (e) => {
+    e.preventDefault();
+    const name = inputname.value;
+    const email = inputemail.value;
+    if (name && email) {
+        localStorage.setItem('Name', name);
+        localStorage.setItem('Email', email);
+    }
+};
+formst.addEventListener('submit', Localstore);
