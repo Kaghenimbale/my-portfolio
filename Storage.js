119cd862bc2fffae134604139fe6ef3edb626519
Kagheni <Kaghenimbale@gmail.com>
2023-02-02 17:55:10 +0200

    Ckeck for the linters and arrange storage.js file

--- Storage.js
+++ Storage.js
@@ -3,12 +3,12 @@ const inputname = document.getElementById('name');
 const formst = document.getElementById('form');
 
 const Localstore = (e) => {
-    e.preventDefault();
-    const name = inputname.value;
-    const email = inputemail.value;
-    if (name && email) {
-        localStorage.setItem('Name', name);
-        localStorage.setItem('Email', email);
-    }
+  e.preventDefault();
+  const name = inputname.value;
+  const email = inputemail.value;
+  if (name && email) {
+    localStorage.setItem('Name', name);
+    localStorage.setItem('Email', email);
+  }
 };
 formst.addEventListener('submit', Localstore);
