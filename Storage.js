c749e3ad5eff71471c2fe31b06bc24c78b40cc24
Kagheni <Kaghenimbale@gmail.com>
2023-02-03 07:42:56 +0200

    Add algorithm to get data from the local storage

--- Storage.js
+++ Storage.js
@@ -1,20 +1,32 @@
 const inputemail = document.getElementById('email');
 const inputname = document.getElementById('name');
 const formst = document.getElementById('form');
-const arr = [];
+const textarea = document.getElementById('textarea');
+
 const Localstore = (e) => {
   e.preventDefault();
   const name = inputname.value;
   const email = inputemail.value;
+  const message = textarea.value;
 
   const obj = {};
 
   obj.Name = name;
   obj.Email = email;
+  obj.Message = message;
 
   const result = JSON.stringify(obj);
-  arr.push(result);
-  localStorage.setItem('data', JSON.stringify(arr));
+  localStorage.setItem('data', result);
 };
 
 formst.addEventListener('submit', Localstore);
+
+const getdatastored = JSON.parse(localStorage.getItem('data'));
+
+window.onload = () => {
+  if (getdatastored) {
+    inputname.value = getdatastored.Name;
+    inputemail.value = getdatastored.Email;
+    textarea.value = getdatastored.Message;
+  }
+};
